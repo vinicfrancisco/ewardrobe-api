83ff72186bb981bed50194ddcd66b63bc6c047aa
vinicfrancisco <vini.ibi@gmail.com>
2020-07-17 08:33:49 -0300

    created many to many pivot table

---
+++ database/migrations/1594985548037_looks_clothes_schema.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema');
+
+class LooksClothesSchema extends Schema {
+  up() {
+    this.create('looks_clothes', table => {
+      table.uuid('id').primary();
+      table
+        .uuid('look_id')
+        .references('id')
+        .inTable('looks')
+        .onUpdate('CASCADE')
+        .onDelete('SET NULL');
+      table
+        .uuid('clothes_id')
+        .references('id')
+        .inTable('clothes')
+        .onUpdate('CASCADE')
+        .onDelete('SET NULL');
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop('looks_clothes');
+  }
+}
+
+module.exports = LooksClothesSchema;
