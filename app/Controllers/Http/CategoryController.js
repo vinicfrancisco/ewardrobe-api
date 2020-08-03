1c2fbd9983cc3659b6a132c0503fafd29cbdd784
vinicfrancisco <vini.ibi@gmail.com>
2020-08-03 08:28:23 -0300

    added categories routes and controllers

---
+++ app/Controllers/Http/CategoryController.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const { uuid } = use('uuidv4');
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Category = use('App/Models/Category');
+
+const AppError = use('App/Exceptions/AppError');
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/auth/src/Schemes/Session')} AuthSession */
+
+class CategoryController {
+  /**
+   * Store  a new category.
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store({ request, response }) {
+    const data = request.body;
+
+    const findCategory = await Category.findBy('name', data.name);
+
+    if (findCategory) {
+      throw new AppError(
+        `Category {${findCategory.name}} already exists`,
+        403,
+        'CategoryAlreadyExists'
+      );
+    }
+
+    const category = await Category.create({
+      id: uuid(),
+      ...data
+    });
+
+    return response.send(category);
+  }
+
+  /**
+   * List all categories and its clothes.
+   * @param {object} ctx
+   * @param {Response} ctx.response
+   */
+  async index({ response }) {
+    const categories = await Category.query().with('clothes').fetch();
+
+    return response.send(categories);
+  }
+}
+
+module.exports = CategoryController;
--- start/routes/index.js
+++ start/routes/index.js
@@ -1,4 +1,29 @@
 'use strict';
 
-// require('./private');
-require('./public');
+/** @type {import('@adonisjs/framework/src/Route/Manager'} */
+const Route = use('Route');
+
+Route.group(() => {
+  /**
+   * HealthCheck
+   */
+  Route.get('healthcheck', ({ response }) => {
+    return response.status(200).send('Health is OK!');
+  });
+
+  /**
+   * AuthController routes.
+   */
+  Route.post('auth', 'AuthController.store');
+
+  /**
+   * UserController routes.
+   */
+  Route.post('users', 'UserController.store');
+
+  /**
+   * CategoryController routes.
+   */
+  Route.post('categories', 'CategoryController.store');
+  Route.get('categories', 'CategoryController.index');
+});
