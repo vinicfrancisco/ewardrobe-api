7ea2b5f592909be1a32bd4d96bf51296346b8fec
Vinícius Catafesta Francisco <vinicfrancisco@gmail.com>
2020-09-18 17:09:59 -0300

    fixed category clothes queries

--- app/Controllers/Http/CategoryClothesController.js
+++ app/Controllers/Http/CategoryClothesController.js
@@ -10,19 +10,11 @@ const Clothes = use('App/Models/Clothes');
 class CategoryClothesController {
   async index({ request, response }) {
     const topClothes = await Clothes.query()
-      .with('category', builder =>
-        builder.where({
-          type: 'top'
-        })
-      )
+      .where('category_id', 'b380a5f7-2f31-4085-989b-785a98aadc58')
       .fetch();
 
     const bottomClothes = await Clothes.query()
-      .with('category', builder =>
-        builder.where({
-          type: 'bottom'
-        })
-      )
+      .where('category_id', 'dfd8421e-43cb-45e2-93a6-c9b4e970a177')
       .fetch();
 
     return response.send({
